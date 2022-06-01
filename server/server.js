cfa4fc5fe092c6c69c2c2777bdce80596a77b78d
Thibault Rucher <thib.ruch@laposte.net>
2022-06-01 10:41:16 +0200

    link the service worer

--- server/server.js
+++ server/server.js
@@ -9,6 +9,12 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "../build")));
 
+app.use('/', express.static('../build', {
+  setHeaders: (res) => {
+      res.setHeader('Service-Worker-Allowed', '/');
+    },
+}));
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../build", "index.html"));
 });
